23aa635b7ba49787d0c97f4622cdcf593fbb5263
ashleyjross <ashley.jacob.ross@gmail.com>
2026-09-03 16:54:29 -0400

    Update get_intile_coverage_mkran.py

--- scripts/get_intile_coverage_mkran.py
+++ scripts/get_intile_coverage_mkran.py
@@ -5,21 +5,36 @@ will be observed by the grism with the assumed wavelength coverage
 requires that the optical model be installed and the environment variable
 ROMAN_GDPS_OPTICAL_MODEL_CONFIG be set to the path of the yaml config file
 for the optical model
-Example run, TBC, some details are still NERSC specific,:
-module load conda
-conda activate /global/common/software/m4943/grizli1
-export github_dir=/global/common/software/m4943/grizli0/
-export PYTHONPATH to include observing-program/py,
-optical_model_tools/py, and GDPS_optical_model/
-export ROMAN_GDPS_OPTICAL_MODEL_CONFIG to the Roman_grism_OpticalModel_v0.8
-yaml file under GDPS_optical_model/roman_gdps_optical_model/config/
-run get_intile_coverage_inDESIran.py with --nran 1 --ramin 49 --ramax 51
---decmin -11 --decmax -9
-to run the full survey
-run get_intile_coverage_inDESIran.py with --fullsurvey
-exposure ID and detector number should be added to the output for a file
-containing each observation of each point, but this is not implemented yet
-A bigger piece would be to get all of the pixel information along each trace. This would be a bit of work and hard to make fast.
+Also requires astropy to be installed
+
+***
+Example install and run on SMDC east:
+Followed instructions here for miniforge3 to allow conda: https://github.com/RomanSpaceTelescope/RST_SMDC_users/blob/main/documentation/miniforge.md
+Needed to add to end of ~/.bashrc file to allow local installs to be found
+export PATH="$HOME/.local/bin:$PATH"
+
+conda create --name test_footprint python
+conda activate test_footprint
+python -m pip install astropy
+
+***github directory at /data/ajross/github/, change below to wherever you want to clone the repos
+export github_dir=/data/ajross/github
+
+cd $github_dir
+git clone https://github.com/RomanSpaceTelescope/GDPS_optical_model.git
+cd GDPS_optical_model/
+python -m pip install -e .
+
+cd $github_dir
+git clone https://github.com/roman-grs-pit/Roman_GRS_footprint.git
+cd Roman_GRS_footprint/
+python -m pip install -e .
+
+export ROMAN_GDPS_OPTICAL_MODEL_CONFIG=$github_dir/GDPS_optical_model/roman_gdps_optical_model/config/Roman_grism_OpticalModel_v0.8.yaml
+
+python $github_dir/Roman_GRS_footprint/scripts/get_intile_coverage_mkran.py
+
+to run the full survey run with --fullsurvey
 """
 
 from rstgrs_footprint import sky_coords, trace_on_det
@@ -42,7 +57,8 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 
 # create formatter
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 # add formatter to ch
 ch.setFormatter(formatter)
@@ -52,18 +68,25 @@ logger.addHandler(ch)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--wficen", help="if y, positions are detector center", default="y")
-parser.add_argument("--wavmin", help="set minimum wavelength, if not None", default=None)
-parser.add_argument("--wavmax", help="set maximum wavelength, if not None", default=None)
-parser.add_argument("--chunksize", help="objects to process per chunk", default=10000000, type=int)
+parser.add_argument(
+    "--wficen", help="if y, positions are detector center", default="y")
+parser.add_argument(
+    "--wavmin", help="set minimum wavelength, if not None", default=None)
+parser.add_argument(
+    "--wavmax", help="set maximum wavelength, if not None", default=None)
+parser.add_argument(
+    "--chunksize", help="objects to process per chunk", default=10000000, type=int)
 parser.add_argument(
     "--tilefile",
     help="full path to tile file",
     default=None,
 )
-parser.add_argument("--nran", help="number of randoms to use", default=10000, type=int)
-parser.add_argument("--outroot", help="root directory for output", default=os.getcwd())
-parser.add_argument("--outname", help="additional name for output files", default="test")
+parser.add_argument("--nran", help="number of randoms to use",
+                    default=10000, type=int)
+parser.add_argument(
+    "--outroot", help="root directory for output", default=os.getcwd())
+parser.add_argument(
+    "--outname", help="additional name for output files", default="test")
 parser.add_argument("--ramin", help="minimum ra", default=49, type=float)
 parser.add_argument("--ramax", help="maximum ra", default=51, type=float)
 parser.add_argument("--decmin", help="minimum dec", default=-11, type=float)
@@ -83,21 +106,28 @@ parser.add_argument(
     help="If set, override any ra,dec bounds and simulate the whole survey",
     action="store_true",
 )
-parser.add_argument("--palist", help="list of PA values for repeated RA,DEC", default=None, type=str, nargs="*")
-parser.add_argument("--radiff", help="diff in RA for the repeated values", default=0, type=float)
-parser.add_argument("--decdiff", help="diff in DEC for the repeated values", default=0, type=float)
-parser.add_argument("--decpa", help="add diff in DEC for the flipped roll angles", default=0, type=float)
-parser.add_argument("--par", help="whether to use the parallel processing", action="store_true")
+parser.add_argument("--palist", help="list of PA values for repeated RA,DEC",
+                    default=None, type=str, nargs="*")
+parser.add_argument(
+    "--radiff", help="diff in RA for the repeated values", default=0, type=float)
+parser.add_argument(
+    "--decdiff", help="diff in DEC for the repeated values", default=0, type=float)
+parser.add_argument(
+    "--decpa", help="add diff in DEC for the flipped roll angles", default=0, type=float)
+parser.add_argument(
+    "--par", help="whether to use the parallel processing", action="store_true")
 args = parser.parse_args()
 
 if args.tilefile is None:
     github_dir = os.environ.get("github_dir")
     if github_dir is None:
         parser.error("--tilefile is required when github_dir is not set")
-    args.tilefile = os.path.join(github_dir, "Roman_GRS_footprint", "data", "994-hlwas-Feb26_fixed_orients.sim.ecsv")
+    args.tilefile = os.path.join(
+        github_dir, "Roman_GRS_footprint", "data", "994-hlwas-Feb26_fixed_orients.sim.ecsv")
 
 if args.fullsurvey:
-    logger.info("fullsurvey flag is set, will ignore any ra,dec bounds and simulate the whole survey")
+    logger.info(
+        "fullsurvey flag is set, will ignore any ra,dec bounds and simulate the whole survey")
     # no Roman footprint seems to go out of these bounds
     decm = -60
     decx = 10
@@ -121,10 +151,12 @@ else:
     )
 data = []
 
-ran4degfn = args.outroot + "/DESIran" + str(args.nran) + str(ram) + str(rax) + str(decm) + str(decx) + ".fits"
+ran4degfn = args.outroot + "/DESIran" + \
+    str(args.nran) + str(ram) + str(rax) + str(decm) + str(decx) + ".fits"
 
 data = Table()
-ra, dec = sky_coords.generate_randoms(nran=args.nran, ra_bounds=(ram, rax), dec_bounds=(decm, decx))
+ra, dec = sky_coords.generate_randoms(
+    nran=args.nran, ra_bounds=(ram, rax), dec_bounds=(decm, decx))
 data["RA"] = ra
 data["DEC"] = dec
 data["ID"] = np.arange(len(data)).astype(int)
@@ -141,7 +173,8 @@ if args.wficen != "y":
 if args.fullsurvey:
     outdir = args.outroot + "/test" + "/fullsurvey" + wfistr + "/"
 else:
-    outdir = args.outroot + "/test" + "/ramin" + str(ram) + "decmin" + str(decm) + wfistr + "/"
+    outdir = args.outroot + "/test" + "/ramin" + \
+        str(ram) + "decmin" + str(decm) + wfistr + "/"
 logger.info("results will be written to " + outdir)
 if not os.path.exists(outdir):
     os.makedirs(outdir)
@@ -173,7 +206,8 @@ logger.info("number of grism tiles in tiling file is " + str(len(tls)))
 
 if args.palist is not None:  # this will only work for medium targets
     palist = np.array(args.palist).astype(float)
-    logger.info("palist provided, will set PA of repeated RA,DEC values to " + str(palist))
+    logger.info(
+        "palist provided, will set PA of repeated RA,DEC values to " + str(palist))
     tlsu = unique(tls, keys=["RA", "DEC"])
     tral = []
     tdecl = []
@@ -200,7 +234,8 @@ if args.fullsurvey is False:
     selreg &= tls["DEC"] < decx + pad
     # print(len(tls[selreg]))
     tls = tls[selreg]
-logger.info("number of grism tiles to simulate after ra/dec cuts is " + str(len(tls)))
+logger.info(
+    "number of grism tiles to simulate after ra/dec cuts is " + str(len(tls)))
 # nobs = np.zeros(len(ral_tot))
 
 
@@ -256,10 +291,12 @@ for chunk in range(0, Nchunk):
         ran_indices_tl = ran_indices[sel1deg]
 
         if len(ran_indices_tl) > 0:
-            xfpa, yfpa = sky_coords.tangent_plane(np.array(ral_tl), np.array(decl_tl), ra0, dec0, pa)
+            xfpa, yfpa = sky_coords.tangent_plane(
+                np.array(ral_tl), np.array(decl_tl), ra0, dec0, pa)
 
             for det in dets:
-                xpixl, ypixl = trace_on_det.optmod.coords.convert_fpa_to_sca(xfpa, yfpa, sca=det)
+                xpixl, ypixl = trace_on_det.optmod.coords.convert_fpa_to_sca(
+                    xfpa, yfpa, sca=det)
                 selp = xpixl > -1000
                 selp &= xpixl < 5088
                 selp &= ypixl > -1000
@@ -269,7 +306,8 @@ for chunk in range(0, Nchunk):
                     yfpai = yfpa[selp][i]
                     test = 0
 
-                    test = trace_on_det.test_foot(xfpai, yfpai, det=det, min_lam_4foot=minwav, max_lam_4foot=maxwav)
+                    test = trace_on_det.test_foot(
+                        xfpai, yfpai, det=det, min_lam_4foot=minwav, max_lam_4foot=maxwav)
                     if test == 1:
                         idx_det = ran_indices_tl[selp][i]
                         idx.append(idx_det)
@@ -300,7 +338,8 @@ for chunk in range(0, Nchunk):
     det_bits_chunk = np.concatenate(det_bits_chunk)
     expid_chunk = np.concatenate(expid_chunk)
     logger.info("length of concatenated array of ids " + str(len(rand_indx)))
-    rans, indices, cnts = np.unique(rand_indx, return_counts=True, return_inverse=True)
+    rans, indices, cnts = np.unique(
+        rand_indx, return_counts=True, return_inverse=True)
     selobs = np.isin(ran_indices, rans)
     if np.array_equal(ran_indices[selobs], rans):
         logger.info("input/final ids are matched in order")
@@ -323,7 +362,8 @@ for chunk in range(0, Nchunk):
     expid_re.append(expid_chunk)
 
     tf = time()
-    logger.info("finished chunk " + str(chunk) + " in " + str(tf - t0) + " out of " + str(Nchunk))
+    logger.info("finished chunk " + str(chunk) + " in " +
+                str(tf - t0) + " out of " + str(Nchunk))
 
 
 ra_all = np.concatenate(ra_all)
@@ -333,7 +373,8 @@ indx_all = np.concatenate(indx_all)
 det_bits_re = np.concatenate(det_bits_re)
 expid_re = np.concatenate(expid_re)
 indx_re = np.concatenate(indx_re)
-logger.info("concatenated data " + str(len(ra_all)) + " data points with at least one observation")
+logger.info("concatenated data " + str(len(ra_all)) +
+            " data points with at least one observation")
 
 tout = Table()
 tout["RA"] = ra_all
